18a9544bff3d02f831f4be530499b0e336ec3fa1
EricaAkos <ericadeku4@gmail.com>
2024-11-25 21:08:55 +0000

    added all files

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let name = prompt("What is your name?");
+let age = prompt("How old are you?");
+let Email = prompt("Your Email address");
+alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email: ${Email}`)
